d06562b429cb9f63a0b749f729f38aba22347c1f
Luna Tsukinashi <system32@live.jp>
2024-10-15 16:32:54 +0900

    food.pyを作成，関数定義・コメントを追加

---
+++ food.py
@@ -0,0 +1,36 @@
+from item import Item
+
+class Food(Item):
+    """
+    Foodクラス
+        得点アイテムの座標とアイコン，状態を管理
+        アイテムは動かない
+
+    Attributes:
+        now_x (int): 現在のx座標
+        now_y (int): 現在のy座標
+        status (bool): アイテムの状態
+        icon (str): 表示アイコン
+
+    Examples:
+        >>> food = Food(6, 7)
+        >>> food.now_x
+        6
+        >>> food.now_y
+        7
+        >>> food.icon
+        '🍒'
+        >>> food.status
+        True
+        >>> isinstance(food, Item)
+        True
+    """
+
+    def __init__(self, x, y) -> None:
+        super().__init__(x, y)
+        pass
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
