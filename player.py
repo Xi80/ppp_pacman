8910c82631d5aba78caaacab9634abf20d477d13
Luna Tsukinashi <system32@live.jp>
2024-11-12 16:38:22 +0900

    無敵状態を管理できるように変更

--- player.py
+++ player.py
@@ -15,11 +15,15 @@ class Player(Item):
         self.next_x(int) : 次の時刻でのx座標
         self.next_y(int) : 次の時刻でのy座標
         self.status(bool) : アイテムの状態（Trueなら存在する、Falseなら存在しない消滅した）
+        self.invincible(bool) : プレイヤーの状態(Trueで無敵モード，Falseで通常モード)
+        self.count(int) : 残りの無敵時間カウント
     """
 
     def __init__(self, x, y) -> None:
         super().__init__(x, y)
         self.icon = "😶"
+        self.invincible = False
+        self.count = 0
         pass
 
     def get_next_pos(self, dir: tuple[int, int]) -> tuple[int, int]:
@@ -27,7 +31,7 @@ class Player(Item):
         移動方向したい方向を受け取り，移動後の座標を返す．
 
         Args:
-            dir(tuple[int, int]: 次に移動する量(xの移動量,yの移動量)，右下方向に正
+            dir(tuple[int, int]): 次に移動する量(xの移動量,yの移動量)，右下方向に正
 
         Returns:
             tuple[int, int]: 移動後の座標
@@ -43,6 +47,61 @@ class Player(Item):
         self.next_x = self.now_x + dir[0]
         self.next_y = self.now_y + dir[1]
         return (self.next_x, self.next_y)
+
+    def set_invincible(self, state: bool):
+        """
+            無敵状態を設定
+
+            Args:
+                state(bool): 無敵状態，Trueで有効
+
+            Returns:
+                None
+
+            Examples:
+                >>> player = Player(4, 4)
+                >>> player.get_invincible()
+                False
+                >>> player.set_invincible(True)
+                >>> player.get_invincible()
+                True
+        """
+
+        self.invincible = state
+
+        if self.invincible is True:
+            self.count = 100
+        else:
+            self.count = 0
+
+    def get_invincible(self) -> bool:
+        """
+            無敵状態を取得
+
+            Args:
+                None
+
+            Returns:
+                bool: 無敵状態，Trueで有効
+
+            Examples:
+                >>> player = Player(4, 4)
+                >>> player.get_invincible()
+                False
+                >>> player.set_invincible(True)
+                >>> player.get_invincible()
+                True
+        """
+        return self.invincible
+
+    def update_pos(self, stuck: bool = False) -> None:
+        if self.invincible is True:
+            if self.count > 0:
+                self.count -= 1
+            if self.count == 0:
+                self.invincible = False
+
+        return super().update_pos(stuck)
 
 
 if __name__ == "__main__":
